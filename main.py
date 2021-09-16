427d29f0704a95dcf5b2b94bee2788a3c67d0eac
KameronIng <kamisda1@outlook.com>
2021-09-16 01:27:56 -0400

    started creation of main screen and account creation screen

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
-# This is a sample Python script.
+# function to pull up main screen
+def main_screen():
+    print("WELCOME TO InCollege!")
+    print("Would you like to create a new account or log into an existing account?")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    # accepts user input will bring them to the appropriate screen
+    user_input = input("(Enter 'n' for new account or 'e' for existing account)")
+
+    if user_input == 'n':
+        # print("call create account function")
+        create_account()
+    else:
+        print("user login screen")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# function to pull up account creation screen
+def create_account():
+    username_ = input("Enter the username you wish to use:")
+    # eventually will check database(file) to make sure username is unique (via 'if' condition)
+
+    # allows user to create a password and validates input with the given conditions
+    condition = True
+    while condition:
+        print("Enter the password you wish to user. Your password must contain: \n",
+        "a minimum of 8 characters\n",
+        "a maximum of 12 characters\n",
+        "at least one capital letter\n",
+        "at least one digit\n",
+        "at least one non-alphabetic character")
+        password_ = input()
+
+        if len(password_) < 8 or len(password_) > 12:
+            continue
+
+        if not password_.isalnum():
+            continue
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Hello World')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
+# PROGRAM START #
+main_screen()
