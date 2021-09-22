4505be2971f6c25881b94318937812696c1bac03
Lyle Hanner <66330100+lynnhanananer@users.noreply.github.com>
2021-09-22 10:21:19 -0400

    Commit reverted

--- account_class.py
+++ account_class.py
@@ -1,9 +1,11 @@
 class Account:
+    # Account class constructor with optional arguments for username, password, firstname and lastname
     def __init__(self, username=None, password=None, firstname=None, lastname=None):
         self.username = username
         self.password = password
         self.firstname = firstname
         self.lastname = lastname
 
-    def get_account_details_list(self):
+    # returns a list of the Account class object properties to be used for writing to the csv file
+    def get_account_details(self):
         return [self.username, self.password, self.lastname, self.firstname]
