f0ffbea77c8ddafedbb5e779c240e91c3e4b2cfc
KameronIng <kamisda1@outlook.com>
2021-09-26 12:57:16 -0400

    added comments for 'create job' test

--- test_main.py
+++ test_main.py
@@ -1,4 +1,4 @@
-import main
+from main import *
 import unittest
 import mock
 import csv
@@ -8,31 +8,33 @@ from account_class import Account
 class TestCases(unittest.TestCase):
     def test_login_screen(self, ):
         test_accounts = [Account('alice', 'Alice123!'), Account('lily', 'J!888888')]
-        assert main.passwd_valid(test_accounts, 'alice', 'Alice123!')
-        assert not main.passwd_valid(test_accounts, 'alice', 'Alice')
-        assert main.passwd_valid(test_accounts, 'lily', 'J!888888')
-        assert not main.passwd_valid(test_accounts, 'lily', 's888')
-        assert not main.passwd_valid(test_accounts, 'lily', '')
+        assert passwd_valid(test_accounts, 'alice', 'Alice123!')
+        assert not passwd_valid(test_accounts, 'alice', 'Alice')
+        assert passwd_valid(test_accounts, 'lily', 'J!888888')
+        assert not passwd_valid(test_accounts, 'lily', 's888')
+        assert not passwd_valid(test_accounts, 'lily', '')
 
     def test_is_secure(self, ):
-        condition, message = main.is_secure('123')
+        condition, message = is_secure('123')
         assert not condition
         assert message == 'Error: password must be a minimum of 8 and a maximum of 12 characters'
-        condition, message = main.is_secure('12345678')
+        condition, message = is_secure('12345678')
         assert not condition
         assert message == "Error: password must contain at least 1 capital letter"
-        condition, message = main.is_secure('ABCDEFGHI')
+        condition, message = is_secure('ABCDEFGHI')
         assert not condition
         assert message == "Error: password must contain at least 1 digit"
-        condition, message = main.is_secure('ab123C...')
+        condition, message = is_secure('ab123C...')
         assert condition and message == ''
 
+    # Run this test with 'jobs.txt' empty; can change assert count accordingly if testing more input
+    # Test checks that the number of jobs in 'jobs.txt' matches the number of jobs created
     def test_create_jobs(self):
         with mock.patch('builtins.input', side_effect=['Software Engineer', 'Developing SW', 'Microsoft', '70000']):
-            main.create_job('Alice')
+            create_job('Alice')
 
         with mock.patch('builtins.input', side_effect=['Project Lead', 'Oversee developers', 'Google', '80000']):
-            main.create_job('John')
+            create_job('John')
 
         count = 0
         with open('jobs.txt') as csv_file:
