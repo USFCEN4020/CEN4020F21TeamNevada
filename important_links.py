eb7e92846d2aca8455b5414e384b6a4039235d77
Lyle Hanner <66330100+lynnhanananer@users.noreply.github.com>
2021-10-01 13:12:15 -0400

    added guest controls and language options and logic

--- important_links.py
+++ important_links.py
@@ -1,4 +1,8 @@
-def important_links_groups(signed_in):
+from account_class import Account
+from csv_read_write import save_accounts_to_csv
+
+
+def important_links_groups(user=None, accounts=None):
     # Prints the InCollege important Links menu
     menu_opt = {"1": "Copyright Notice",
                 "2": "About",
@@ -73,12 +77,138 @@ def important_links_groups(signed_in):
                   ,"\n","screenshots nor other brand features. To be able to use it, members or organizations must obtain prior approval from CollegeIn.")
         elif selection == '9':
             # if the user is signed in, they are allow to adjust the guest controls
-            if signed_in:
-                pass
-            else:
+            if user is None or accounts is None:
                 print("You must be signed in to your InCollege account to access Guest Controls")
+            else:
+                # account controls
+                guest_controls(user, accounts)
+                print("return to important links")
+                for account in accounts:
+                    print(account.get_account_details())
+        elif selection == '10':
+            account_language(user, accounts)
         elif selection == 'q':
             print("\n")
             break
         else:
             print("Invalid Selection, Try Again!")
+
+
+# this function handles the guest controls options
+def guest_controls(user, accounts):
+    menu_opt = {"1": "InCollege Email:",
+                "2": "SMS:",
+                "3": "Targeted Advertising:",
+                "e": "Enable All",
+                "d": "Disabled All",
+                "q": "Return to important links"}
+    menu_condition = True
+    changes = False
+
+    while menu_condition:
+        # get the logged in account's controls
+        account_controls_settings = user.get_account_controls()
+
+        # print the options and controls
+        print("\n********* Guest Controls *********\n")
+        options = menu_opt.keys()
+        for x in options:
+            # if the menu item is a control, print the value for the control state as enabled or disabled
+            try:
+                # the following code tries to get the value of the control state from the list
+                # using some clever logic with the int() function and the fact that the menu
+                # keys are numbers, and then saves the control state as a string
+                control_state = account_controls_settings[int(x)-1]
+                control_state_string = "Enabled" if control_state else "Disabled"
+                print("{0} ) {1:<25}{2}".format(x, menu_opt[x], control_state_string))
+                # print(x, ")", menu_opt[x], control_state_string)
+            except ValueError:
+                # this error will catch the menu options that are not control states and print the menu option
+                print(x, ")", menu_opt[x])
+
+        selection = input("Select an Option to toggle a control, change all controls or save changes: ")
+
+        # if the user opts to change any of the controls, we need to change the
+        # quit menu option text and save the changes to the csv once the user is
+        # done making changes
+
+        if selection == "q":
+            if changes:
+                # since user is a reference to the object in the accounts list, we don't have to change the accounts list
+                save_accounts_to_csv(accounts)
+
+                print("Saving changes to Guest Controls")
+            menu_condition = False
+        elif selection in options:
+            update_guest_controls(selection, user)
+            menu_opt["q"] = "Save changes and return to important links"
+            changes = True
+        else:
+            print("Unknown Selection, Try Again!\n")
+
+
+# this function updates the guest controls for the account based on the value passed as the selection and the Account object
+def update_guest_controls(selection, user):
+    if selection == "1":
+        user.incollege_email = not user.incollege_email
+    elif selection == "2":
+        user.sms = not user.sms
+    elif selection == "3":
+        user.targeted_ads = not user.targeted_ads
+    elif selection == "e":
+        user.incollege_email = True
+        user.sms = True
+        user.targeted_ads = True
+    elif selection == "d":
+        user.incollege_email = False
+        user.sms = False
+        user.targeted_ads = False
+    else:
+        pass
+
+
+def account_language(user, accounts):
+    menu_opt = {"1": "English",
+                "2": "Spanish",
+                "q": "Return to important links"}
+    menu_condition = True
+    changed = False
+    
+    while menu_condition:
+        print("\n********* Language *********\n")       
+        options = menu_opt.keys()
+        for x in options:
+            print(x, ")", menu_opt[x])
+        selection = input("Select a Option to change to the language or save changes: ")
+
+        if selection == 'q':
+            if changed:
+                # since user is a reference to the object in the accounts list, we don't have to change the accounts list
+                save_accounts_to_csv(accounts)
+
+            menu_condition = False
+        elif selection in options:
+            if update_account_language(selection, user):
+                changed = True
+                menu_opt["q"] = "Save changes and return to important links"
+        else:
+            print("Unknown Selection, Try Again!\n")
+
+
+def update_account_language(selection, user):
+    if selection == "1":
+        if user.language == "English":
+            print("Language is already set to English\n")
+            return False
+        else:
+            print("Language set to English\n")
+            user.language = "English"
+            return True
+    elif selection == "2":
+        if user.language == "Spanish":
+            print("Language is already set to Spanish\n")
+            return False
+        else:
+            print("Language set to Spanish\n")
+            user.language = "Spanish"
+            return True
