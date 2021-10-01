86eb604609b2e084a93312330c10cfe5ffcc0f3a
Lyle Hanner <66330100+lynnhanananer@users.noreply.github.com>
2021-10-01 11:27:19 -0400

    added account class properties for the account controls

--- account_class.py
+++ account_class.py
@@ -1,11 +1,58 @@
 class Account:
     # Account class constructor with optional arguments for username, password, firstname and lastname
-    def __init__(self, username=None, password=None, firstname=None, lastname=None):
+    def __init__(self, username=None, password=None, firstname=None, lastname=None, incollege_email=None, sms=None, targeted_ads=None, language=None):
         self.username = username
         self.password = password
         self.firstname = firstname
         self.lastname = lastname
 
+        # logic for account controls defaults
+        if incollege_email is None:
+            # default on account creation
+            self.incollege_email = True
+        elif incollege_email == "True":
+            self.incollege_email = True
+        elif incollege_email == "False":
+            self.incollege_email = False
+        else:
+            # default if true or false not passed as the constructor argument
+            self.incollege_email = True
+            
+        if sms is None:
+            self.sms = True
+        elif sms == "True":
+            self.sms = True
+        elif sms == "False":
+            self.sms = False
+        else:
+            self.sms = True
+    
+        if targeted_ads is None:
+            self.targeted_ads = True
+        elif targeted_ads == "True":
+            self.targeted_ads = True
+        elif targeted_ads == "False":
+            self.targeted_ads = False
+        else:
+            self.targeted_ads = True
+            
+        if language is None:
+            self.language = "English"
+        elif language == "English":
+            self.language = "English"
+        elif language == "Spanish":
+            self.language = "Spanish"
+        else:
+            self.language = "English"
+
     # returns a list of the Account class object properties to be used for writing to the csv file
     def get_account_details(self):
-        return [self.username, self.password, self.lastname, self.firstname]
+        return [self.username, self.password, self.lastname, self.firstname, self.incollege_email, self.sms, self.targeted_ads, self.language]
+
+    # returns a list of the Account class object properties for the guest controls
+    def get_account_controls(self):
+        return [self.incollege_email, self.sms, self.targeted_ads]
+
+    # returns the Account's selected language
+    def get_account_language(self):
+        return self.language
