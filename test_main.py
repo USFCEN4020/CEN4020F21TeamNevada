9a69621fe72040f1ac67eb877a182834aeccb99a
Lyle Hanner <66330100+lynnhanananer@users.noreply.github.com>
2021-10-03 21:53:06 -0400

    pushing up testing changes for kameron

--- test_main.py
+++ test_main.py
@@ -30,13 +30,16 @@ class TestCases(unittest.TestCase):
     def test_main_screen(self):
         test_accounts = [Account('john', 'John123!', 'John', 'Doe'), Account('mark', 'Mark123!', 'Mark', 'Smith')]
 
+        print("-----TEST OPTION 'l'-----")
         with mock.patch('builtins.input', side_effect=['3', 'John', 'Doe', 'l', 'mark', 'Mark123!', 'q']):
             main_screen(test_accounts)
 
+        print("-----TEST OPTION 's'-----")
         with mock.patch('builtins.input', side_effect=['3', 'John', 'Doe', 's', 'sally', 'Hill123!', 'Sally', 'Hill',
                                                        'sally', 'Hill123!', 'q']):
             main_screen(test_accounts)
 
+        print("-----TEST OPTION 'q'-----")
         with mock.patch('builtins.input', side_effect=['3', 'John', 'Doe', 'q', 'q']):
             main_screen(test_accounts)
 
@@ -67,6 +70,79 @@ class TestCases(unittest.TestCase):
     def test_home_screen(self):
         with mock.patch('builtins.input', side_effect=['1', '\n', 'x', '2']):
             home_screen()
+
+    # Tests the first option (general links) of the useful links functionality
+    def test_useful_links_groups_option1(self):
+        test_accounts = [Account('john', 'John123!', 'John', 'Doe'), Account('mark', 'Mark123!', 'Mark', 'Smith')]
+
+        # This test additionally checks to make sure that invalid user input is handled correctly (i.e user inputs 'x')
+        print("-----TEST_OPTION 1.1-----")
+        with mock.patch('builtins.input', side_effect=['4', 'x', '1', 'x', '1', 'sally', 'Hill123!', 'Sally', 'Hill',
+                                                       'sally', 'Hill123!', 'q']):
+            main_screen(test_accounts)
+
+        print("-----TEST_OPTION 1.2-----")
+        with mock.patch('builtins.input', side_effect=['4', '1', '2', 'q', 'q', 'q']):
+            main_screen(test_accounts)
+
+        print("-----TEST_OPTION 1.3-----")
+        with mock.patch('builtins.input', side_effect=['4', '1', '3', 'q', 'q', 'q']):
+            main_screen(test_accounts)
+
+        print("-----TEST_OPTION 1.4-----")
+        with mock.patch('builtins.input', side_effect=['4', '1', '4', 'q', 'q', 'q']):
+            main_screen(test_accounts)
+
+        print("-----TEST_OPTION 1.5-----")
+        with mock.patch('builtins.input', side_effect=['4', '1', '5', 'q', 'q', 'q']):
+            main_screen(test_accounts)
+
+        print("-----TEST_OPTION 1.6-----")
+        with mock.patch('builtins.input', side_effect=['4', '1', '6', 'q', 'q', 'q']):
+            main_screen(test_accounts)
+
+        print("-----TEST_OPTION 1.7-----")
+        with mock.patch('builtins.input', side_effect=['4', '1', '7', 'q', 'q', 'q']):
+            main_screen(test_accounts)
+
+    # Tests options 2 through 4 of the useful links functionality
+    def test_useful_links_groups_options2_thru_4(self):
+        test_accounts = [Account('john', 'John123!', 'John', 'Doe'), Account('mark', 'Mark123!', 'Mark', 'Smith')]
+
+        # This test additionally checks to make sure that invalid user input is handled correctly (i.e user inputs 'x')
+        print("-----TEST_OPTION 2-----")
+        with mock.patch('builtins.input', side_effect=['4', 'x', '2', 'q', 'q']):
+            main_screen(test_accounts)
+
+        print("-----TEST_OPTION 3-----")
+        with mock.patch('builtins.input', side_effect=['4', '3', 'q', 'q']):
+            main_screen(test_accounts)
+
+        print("-----TEST_OPTION 4-----")
+        with mock.patch('builtins.input', side_effect=['4', '4', 'q', 'q']):
+            main_screen(test_accounts)
+
+    # Tests that each guest control can be properly enabled and disabled
+    def test_guest_controls(self):
+        test_accounts = [Account('john', 'John123!', 'John', 'Doe'), Account('mark', 'Mark123!', 'Mark', 'Smith')]
+
+        # This test additionally checks to make sure that invalid user input is handled correctly (i.e user inputs 'x')
+        print("-----TEST_OPTION 1-----")
+        with mock.patch('builtins.input', side_effect=['2', 'john', 'John123!', '5', 'x', '9', 'x', '1', '1', 'q', 'q', 'q']):
+            main_screen(test_accounts)
+
+        print("-----TEST_OPTION 2-----")
+        with mock.patch('builtins.input', side_effect=['2', 'john', 'John123!', '5', '9', '2', '2', 'q', 'q', 'q']):
+            main_screen(test_accounts)
+
+        print("-----TEST_OPTION 3-----")
+        with mock.patch('builtins.input', side_effect=['2', 'john', 'John123!', '5', '9', '3', '3', 'q', 'q', 'q']):
+            main_screen(test_accounts)
+
+        # This test checks that 'enable all' and 'disable all' work correctly
+        print("-----TEST_OPTIONS 'd'+'e'-----")
+        with mock.patch('builtins.input', side_effect=['2', 'john', 'John123!', '5', '9', 'd', 'e', 'q', 'q', 'q']):
+            main_screen(test_accounts)
 
 
 if __name__ == '__main__':
