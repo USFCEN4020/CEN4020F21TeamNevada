b7c759f69b351b69af5fdb25e8776d56e3c4e222
printyja <70492660+printyja@users.noreply.github.com>
2021-10-07 16:01:54 -0400

    Finished up Profile/Job experience section

--- profile_creation.py
+++ profile_creation.py
@@ -1,10 +1,5 @@
 import string
 
-"""
-TO DO: (Yinet)
-1. Finish up Experience section 
-2. Make paragraph look certain way
-"""
 
 def profile_creation():
     print("********* Profile Creation *********")
@@ -23,33 +18,11 @@ def profile_creation():
     print("Enter information about you: ")
     about_ = input()
 
-    """print("Enter job experience (y/n)")
-    choice = ' '"""
-
-    print("********* Provide Education information *********")
-    print("Enter school name")
-    attended_school = input()
-    print("Enter degree")
-    degree_ = input()
-    print("Enter Years attended: ")
-    years_attended = input()
-
-    print("\n",title_, "\n", capital_major,"\n", capital_university, "\n", about_ )
-""" if choice == 'y':
-        job_experience()
-    elif choice == 'n':
-        print("\n")
-    # break
-    else:
-        print("\nUnknown Selection. Try Again!\n")
-"""
-    print("\n",attended_school,"\n", degree_,"\n", years_attended)
-"""
-def job_experience():
-    print("You may enter up to 3 jobs. How many past jobs would you like to enter:")
-    job_num = input()
-    if (1 <= job_num) or (job_num <= 3):
-        for i in range(job_num):
+    print("Enter experience (y/n)")
+    choice = input()
+    if choice == "y":
+        jobs = []
+        for i in range(3):
             print("Enter job title: ")
             title_ = input()
             print("Enter Employer: ")
@@ -62,8 +35,31 @@ def job_experience():
             location_ = input()
             print("Enter job description: ")
             description_ = input()
-            print(title_, employer_, start_date, end_date, location_, description_)
-        else:
-            print("Please, enter up to 3 jobs: ")
-        # break
+            print("Would you like to enter another job? (y/n)")
+            data = input()
+            if str.lower(data) == "y":
+                print("Enter another job")
+                continue
+            elif str.lower(data) == "n":
+                break
+            else:
+                print("Invalid entry.\n")
+            jobs.append(data)
+    elif choice == "n":
+        print("\n")
+    else:
+        print("Invalid Entry")
+
+    print("********* Provide Education information *********")
+    print("Enter school name")
+    attended_school = input()
+    print("Enter degree")
+    degree_ = input()
+    print("Enter Years attended: ")
+    years_attended = input()
+
 """
+    print("\n",title_, "\n", capital_major,"\n", capital_university, "\n", about_ ) #Tentative for Dogni
+    print("\n",attended_school,"\n", degree_,"\n", years_attended) #Tentative for Dogni
+"""
+
