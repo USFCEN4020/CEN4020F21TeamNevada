9cacfadd41f94a76ca6e934d21afb84b2c0c8c86
NickH2017 <NicolasHernandezOne@gmail.com>
2021-10-20 20:37:35 -0400

    Added functionality to View Saved jobs

--- job_apply.py
+++ job_apply.py
@@ -6,6 +6,8 @@ import json
 def apply_job(user, jobs):
     menu_opt = {"1": "Get a Job's Details",
                 "2": "Apply For a Job",
+                "3": "Save a Job for Later",
+                "4": "View saved Jobs",
                 "q": "Return"}
 
     job_list = []
@@ -19,6 +21,14 @@ def apply_job(user, jobs):
     for x in contents:
         if x['applicant'] == user.username:
             applied_list.append(x['job_title'])
+    
+    with open("jobs_saved.json", "r") as fs:
+        saved_jobs = json.loads(fs.read())
+
+    saved_list = []
+    for x in saved_jobs:
+        if x['user'] == user.username:
+            saved_list.append(x['job_title'])
 
     while True:
         if len(jobs) == 0:
@@ -42,9 +52,21 @@ def apply_job(user, jobs):
 
             selection = input("Select an Option: ")
 
+            # Display Job Information selection
             if selection == '1':
-                pass
-                # Tentative for "Display Job Information" story
+                selection = input("Select to view Details: ")
+                if selection not in job_list:
+                    print("That job does not exist!")
+                elif selection in job_list:
+                    for x in jobs:
+                        if selection == x.title:
+                            print("\n *********",selection,"********* \n")
+                            print("Description:",x.description)
+                            print("Employeer: ",x.employer)
+                            print("Location: N/A")
+                            print("Salary: ",x.salary,"\n")
+                            break       
+            # Apply for a  selection    
             elif selection == '2':
                 selection = input("Select a job to apply for: ")
 
@@ -110,7 +132,63 @@ def apply_job(user, jobs):
                                 user.num_applied = user.num_applied + 1
 
                                 break
+            # Save for later Job Selection
+            elif selection == '3':
+                selection = input("Select a job to save: ")
+                if selection not in job_list:
+                    print("That job does not exist!")
+                elif selection in job_list and selection not in saved_list:
+                    job = {
+                                "job_title": selection,
+                                "user": user.username,
+                                }
+                    saved_jobs.append(job)
+                    with open("jobs_saved.json", "w") as fs:
+                        json.dump(saved_jobs, fs)
+                    break
+                else:
+                    print("You have already saved this job!")
+                    break
+            # View all saved Jobs
+            elif selection == '4':
+                menu_sav = {"1":"Unsave Job",
+                            "q":"return"}
 
+                print("\n ********* Saved Jobs ********* \n")
+                for job in saved_list:
+                    if job in applied_list:
+                        print(job, "APPLIED")
+                    else:
+                        print(job)
+                
+                options = menu_sav.keys()
+                for x in options:
+                    print(x, ")", menu_sav[x])
+
+                selection = input("Select an Option: ")
+
+                if selection == '1':
+                    selection = input("Unsave job: ")
+                    if selection not in saved_list:
+                        print("Job is not already saved!")
+                    else:
+                        # JSON File popping
+                        for i in range(len(saved_jobs)):
+                            if saved_jobs[i]['job_title'] == selection and saved_jobs[i]['user'] == user.username:
+                                saved_jobs.pop(i)
+                                break
+
+                        with open("jobs_saved.json", "w") as fs:
+                            json.dump(saved_jobs, fs)
+
+                        print("Job", selection, "removed from saved list.")
+                        break
+                elif selection == 'q':
+                    break
+                else:
+                    print("Unknown Selection! Try Again!")
+                
+            #  Quit
             elif selection == 'q':
                 break
             else:
