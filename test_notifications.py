8044a0fc5f6afe7d20f62b0435a75a55292bd4ac
NickH2017 <NicolasHernandezOne@gmail.com>
2021-11-07 09:26:34 -0500

    Test cases added

--- test_notifications.py
+++ test_notifications.py
@@ -2,11 +2,29 @@ import unittest
 import mock
 from job_class import Job
 from account_class import Account
+from profile_class import Profile
 from job_apply import *
 from job_deletion import *
 
 class TestCases(unittest.TestCase):
-    pass
+    
+    def test_notification_currently_applied(self):
+        pass
+
+    def test_notification_new_job_posted(self):
+        pass
+
+    def test_notification_deleted_applied_job(self):
+        pass
+
+    def test_notifcation_new_student_join(self):
+        pass
+
+    def test_notifcation_apply_to_jobs(self):
+        pass
+
+    def test_notifcation_new_message(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
