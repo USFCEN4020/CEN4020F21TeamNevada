5245685bc07363ceaa527548a6cd2a8f59554ef7
NickH2017 <NicolasHernandezOne@gmail.com>
2021-11-07 09:11:56 -0500

    Added Testing File for Notifications

---
+++ test_notifications.py
@@ -0,0 +1,12 @@
+import unittest
+import mock
+from job_class import Job
+from account_class import Account
+from job_apply import *
+from job_deletion import *
+
+class TestCases(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
